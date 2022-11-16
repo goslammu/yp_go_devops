8d0b27377610c39f533e86ed8a841ecd45cb0b68
dcaiman <77855475+dcaiman@users.noreply.github.com>
2022-11-16 16:41:13 +0300

    fixed filestorage updateMetric non-locked method

--- internal/pkg/filestorage/filestorage.go
+++ internal/pkg/filestorage/filestorage.go
@@ -60,10 +60,6 @@ func (st *fileStorage) UpdateMetric(m *metric.Metric) error {
 	st.Lock()
 	defer st.Unlock()
 
-	return st.updateMetric(m)
-}
-
-func (st *fileStorage) updateMetric(m *metric.Metric) error {
 	if m == nil {
 		return metric.ErrCannotUpdateInvalidFormat
 	}
@@ -86,11 +82,8 @@ func (st *fileStorage) updateMetric(m *metric.Metric) error {
 
 // Updates metrics collected in input batch by valuable fields: overrides Values and increments Deltas.
 func (st *fileStorage) UpdateBatch(batch []*metric.Metric) error {
-	st.Lock()
-	defer st.Unlock()
-
 	for i := range batch {
-		if err := st.updateMetric(batch[i]); err != nil {
+		if err := st.UpdateMetric(batch[i]); err != nil {
 			return err
 		}
 	}
@@ -110,8 +103,6 @@ func (st *fileStorage) AccessCheck() error {
 
 // Uploads storage to json-file on path defined in constructor.
 func (st *fileStorage) UploadStorage() error {
-	var err error
-
 	st.Lock()
 	defer st.Unlock()
 
@@ -144,9 +135,6 @@ func (st *fileStorage) UploadStorage() error {
 
 // Downlod storage from json-file on path defined in constructor.
 func (st *fileStorage) DownloadStorage() error {
-	st.Lock()
-	defer st.Unlock()
-
 	file, err := os.Open(st.FilePath)
 	if err != nil {
 		return err
@@ -166,7 +154,7 @@ func (st *fileStorage) DownloadStorage() error {
 			return err
 		}
 
-		if err := st.updateMetric(&m); err != nil {
+		if err := st.UpdateMetric(&m); err != nil {
 			return err
 		}
 	}
